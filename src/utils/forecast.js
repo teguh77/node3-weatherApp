68323f407259584314386be08261ce37ac8759a5
Tama <teguhpratama337@gmail.com>
2019-12-10 11:45:08 +0700

    add forecast data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -12,13 +12,17 @@ const forecast = (latitude, longitude, callback) => {
     } else if (body.error) {
       callback('Unable to find location', undefined);
     } else {
+      console.log(body.daily.data[0]);
       callback(
         undefined,
         body.daily.data[0].summary +
           'it is currently ' +
           body.currently.temperature +
-          ' degrees out. ' +
-          'There is a ' +
+          ' degrees out. This high today is ' +
+          body.daily.data[0].temperatureHigh +
+          ' with a low of ' +
+          body.daily.data[0].temperatureLow +
+          '. There is a ' +
           body.currently.precipProbability +
           '% chance of rain.'
       );
